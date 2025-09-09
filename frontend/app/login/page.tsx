d2fde25180d98b390c133d3611b4b3f2b29a3ac8
KwakJakyeong <ggyul847@gmail.com>
2025-09-09 11:36:31 +0900

    Init

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+import styles from "./page.module.css";
+
+export default function login(){
+  const [topTitle, setTopTitle] = useState("돌아오신 것을 환영합니다.");
+
+  return(
+    <>
+      
+    </>
+  )
+}
