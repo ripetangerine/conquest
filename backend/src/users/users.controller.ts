3e34481908776a58337c9a56a7d87db562ec937f
KwakJakyeong <ggyul847@gmail.com>
2025-10-28 02:51:36 +0900

    feature(backend/users) : crud

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from 'src/dto/user.dto';
+import { UserEntity } from './entities/user.entitiy';
+
+@Controller('users')
+export class UsersController {
+  constructor(
+    private readonly usersService: UsersService
+  ){}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserEntity>{
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id:number): Promise<UserEntity>{
+    return this.usersService.findOne(id);
+  }
+
+
+  // 사용자 수정기능 뺌
+  // 추후 사용자 프로필 이미지 수정기능 추가
+
+}
---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,35 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { UserEntity } from './entities/user.entitiy';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from 'src/dto/user.dto';
+
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly usersRepository: Repository<UserEntity>,
+  ){}
+
+  // 사용자 생성
+  async create(
+    createUserDto: CreateUserDto
+  ) : Promise<UserEntity>{
+    const newUser = this.usersRepository.create(createUserDto);
+    return await this.usersRepository.save(newUser);
+  }
+
+  async findOne(id: number): Promise<UserEntity>{
+    try{
+      return await this.usersRepository.findOneOrFail({where: {id}});
+    }catch{
+      throw new NotFoundException(`not found user id : ${id}`);
+    }
+  }
+
+  async update(id: number, updateData:Partial<CreateUserDto>) : Promise<UserEntity>{
+    await this.usersRepository.update(id, updateData);
+    return this.findOne(id);
+  }
+}
