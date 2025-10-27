661b110f494af89ace0905d08b79a263732ab11e
KwakJakyeong <ggyul847@gmail.com>
2025-10-28 02:51:57 +0900

    chore(backend/post)

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEntity } from './entities/post.entity';
 
 @Module({
+  // 엔티티 인식하도록 모듈 등록
+  imports: [TypeOrmModule.forFeature([PostEntity])],
   providers: [PostService],
   controllers: [PostController]
 })
