8f4d7c7e64950788f87de37586fbec91c0809f82
KwakJakyeong <ggyul847@gmail.com>
2025-10-28 12:41:30 +0900

    fix(backend): env mode(prod, dev) error .env.dev -> .env

--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -3,10 +3,20 @@ import { PostService } from './post.service';
 import { PostController } from './post.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from './entities/post.entity';
+import { PostImageEntity } from './entities/post-image.entity';
+import { PostTagEntity } from './entities/post-tag.entity';
 
 @Module({
   // 엔티티 인식하도록 모듈 등록
-  imports: [TypeOrmModule.forFeature([PostEntity])],
+  imports: [
+    TypeOrmModule.forFeature(
+      [
+        PostEntity,
+        PostImageEntity,
+        PostTagEntity,
+      ]
+    )
+  ],
   providers: [PostService,],
   controllers: [PostController],
 })
