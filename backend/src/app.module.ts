b5414626478c5365ed2a972a8abcf9e07bcfa626
KwakJakyeong <ggyul847@gmail.com>
2025-10-28 15:44:48 +0900

    chore(backend)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeormConfig } from './config/typeorm.config';
 import { PostModule } from './post/post.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeormConfig } from './config/typeorm.config';
+import { DataSource } from 'typeorm';
 
 @Module({
   imports: [
@@ -26,4 +27,10 @@ import { AuthModule } from './auth/auth.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(
+    private dataSource: DataSource,
+  ){}
+}
+
+
--- backend/src/post/post.module.ts
+++ backend/src/post/post.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostEntity } from './entities/post.entity';
 import { PostImageEntity } from './entities/post-image.entity';
 import { PostTagEntity } from './entities/post-tag.entity';
