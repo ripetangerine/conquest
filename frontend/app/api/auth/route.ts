c0dd455acf6980fba1d2cf40304331c02c6985ae
KwakJakyeong <ggyul847@gmail.com>
2025-11-04 17:42:50 +0900

    feat(FE): auth route post 회원가입 요청 추가

---
+++ frontend/app/api/auth/route.ts
@@ -0,0 +1,29 @@
+import {NextResponse} from 'next/server';
+
+export async function POST(req: Request){
+  const body = await req.json();
+
+  try{
+    //서버의 응답
+    const res = await fetch(
+      `${process.env.NEST_BASE_URL}/auth/login`,{
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(body),
+      }
+    );
+    const data = await res.json();
+    return NextResponse.json(
+      data,
+      {status: res.status}
+    );
+
+  }catch(error){
+    console.log(`회원가입 에러 : ${error}`);
+    return NextResponse.json(
+      {message: `auth 회원가입 생성 프록시 요청 실패`},
+      {status : 500}
+    )
+  }
+}
+
